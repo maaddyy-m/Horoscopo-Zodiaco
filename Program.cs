dec8bab85daf512f6234f17fe03ab8a6ff0b144b
maaddyy-m <nicoleemartinn712@gmail.com>
2025-06-03 13:55:59 -0300

    Comit principal
    Agregacion de todos lo avanzado anteriormente

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+public static void Main(string[] args)
+{
+    Console.WriteLine("Ingrese su fecha de nacimiento (formato:dd/mm/aaaa)");
+   Fecha fecha = new Fecha;  
+   Caracteristica caracteristica = new caracteristica;
+   Signo signo = new signo;
+   Fecha
+
+} 
+ 
+ public class Zodiaco{
+
+    public abstract void MostrarCaracteristicas ()
+ 
+ }
+
+ public class Aries : Zodiaco{
+
+    public override void MostrarCaracteristicas()
+    {
+        Console.WriteLine("Aries: Energico, valiente, lider natural.");
+    }
+}
+ public class Tauro : Zodiaco {
+    public override void MostrarCaracteristicas()
+    {
+        Console.WriteLine("Tauro: Paciente, confiable, amante de la comodidad. ")
+    }
+}
+  public class Geminis   : Zodiaco {
+    public override void MostrarCaracteristicas()
+    {
+        Console.WriteLine("Gemenis: Comunicativo, adaptable, curioso.");
+    }
+}
+  public class Cancer : Zodiaco {
+
+    public override void MostrarCaracteristicas()
+    {
+        Console.WriteLine("Cancer: Emocional, protector, intuitivo")
+    }
+}
+  public class Leo  : Zodiaco {
+
+ }
+  public class Virgo   : Zodiaco {
+
+ }
+  public class Libra : Zodiaco {
+
+ }
+  public class Escorpio  : Zodiaco {
+
+ }
+  public class  Sagitario : Zodiaco {
+
+ }
+  public class  Capricornio : Zodiaco {
+
+ }
+  public class  Acuario : Zodiaco {
+
+ }
+  public class  Piscis : Zodiaco {
+
+ }
+ 
+ public class Signo{
+
+ }
+ public class Fecha{
+
+ }
+
+ public class Caracteristica{
+
+ }
